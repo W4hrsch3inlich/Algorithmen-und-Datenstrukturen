966226b17318ae664e32894de5cd4a51a3245872
Paul Maier <paul-maier2@gmx.de>
2021-12-20 09:59:45 +0100

    Primgenerator seperiert

--- Strukturen/Baum+.c
+++ Strukturen/Baum+.c
@@ -56,37 +56,6 @@ list treeToList(tree t)
 
 //////////////////////////////////////////////
 
-int nextPrime(int a[], int len, int lp)
-{
-  for(int i = lp; i < len; i+=lp)
-  {
-    a[i] = -1;
-  }
-  while(a[lp] == -1)
-  {
-    lp += 1;
-  }
-  return lp;
-}
-
-int primfac(int a[], int s)
-{
-  int acount = 0;
-  int sieb[s];
-  int p = 2;
-  while(s != 1)
-  {
-    while(s % p == 0)
-    {
-      s = s/p;
-      a[acount] = p;
-      acount++;
-    }
-    p = nextPrime(sieb, s, p);
-  }
-  return acount;
-}
-
 int delmaxr(list* lp,int max)
 {
   if(*lp == NULL)return max;
